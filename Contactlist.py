9e5ba91cfc7f5584ab1da9d8915634bf6e095f4f
ilya <jakmarles123@gmail.com>
2022-09-01 09:09:00 +0300

    added notes

--- Contactlist.py
+++ Contactlist.py
@@ -7,20 +7,20 @@ class Contactlist: # this is the whole contactlist and functions to perfome
     def __init__(self):
         pass
 
-    def add_contact(self, name="", tell=0):
+    def add_contact(self, name="", tell=0): # functtion that adds a contact to contact valubale 
         self.contacts.append(Contact(name, tell))
 
-    def remove_contact(self, victim):
+    def remove_contact(self, victim): # removes a contact and let you chose him by name
         self.contacts.remove(victim)
 
-    def search_contact(self, contact_name):
+    def search_contact(self, contact_name): # search for a contact by using his name
         for contact in self.contacts:
             if type(contact) is Contact:
                 if contact.name == contact_name:
                     return contact
         return None
 
-    def get_contacts_as_json_ar(self):
+    def get_contacts_as_json_ar(self): # makes the inputed string putable in json by format
         res = []
         for contact in self.contacts:
             res.append(json.loads(contact.__str__()))
